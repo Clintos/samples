ccdc545193128ee3faa71b79f55af8f2e5163ea4
Clintos <clintostg@gmail.com>
2022-03-09 11:59:58 -0500

    Add files via upload

---
+++ LevelController.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelController : MonoBehaviour {
+
+    private bool paused;
+    public GameObject enemy;
+    public GameObject wall;
+    private Resetable[] startingObjects;
+    private List<Resetable> resetableObjects;
+    private List<Placeable> placedObjects;
+    public string nextLevelName;
+
+    public void nextLevel()
+    {
+        SceneManager.LoadScene(nextLevelName, LoadSceneMode.Single);
+    }
+
+	// Use this for initialization
+	void Start () {
+        setPaused(true);
+        startingObjects = Object.FindObjectsOfType<Resetable>();
+        resetableObjects = new List<Resetable>();
+        placedObjects = new List<Placeable>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(paused)
+        {
+            if(Input.GetKeyDown(KeyCode.P))
+            {
+                setPaused(false);
+            }
+            else if(Input.GetKeyDown(KeyCode.E))
+            {
+                GameObject _enemy = Instantiate(enemy, transform);
+                _enemy.GetComponent<Placeable>().enabled = true;
+                resetableObjects.Add(_enemy.GetComponent<Resetable>());
+                placedObjects.Add(_enemy.GetComponent<Placeable>());
+            }
+            else if (Input.GetKeyDown(KeyCode.W))
+            {
+                GameObject _wall = Instantiate(wall, transform);
+                _wall.GetComponent<Placeable>().enabled = true;
+                placedObjects.Add(_wall.GetComponent<Placeable>());
+            }
+            else if (Input.GetKeyDown(KeyCode.S))
+            {
+                softReset();
+            }
+            else if (Input.GetKeyDown(KeyCode.R))
+            {
+                hardReset();
+            }
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.P))
+            {
+                setPaused(true);
+            }
+            else if (Input.GetKeyDown(KeyCode.S))
+            {
+                softReset();
+            }
+            else if (Input.GetKeyDown(KeyCode.R))
+            {
+                hardReset();
+            }
+        }
+	}
+
+    public bool getPaused()
+    {
+        return paused;
+    }
+
+    public void setPaused(bool _paused)
+    {
+        paused = _paused;
+        Pauseable[] pauseables = GameObject.FindObjectsOfType<Pauseable>();
+        if(paused)
+        {
+            foreach (Pauseable pauseable in pauseables)
+            {
+                pauseable.setPaused(true);
+            }
+        }
+        else
+        {
+            foreach (Pauseable pauseable in pauseables)
+            {
+                pauseable.setPaused(false);
+            }
+        }
+    }
+
+    public void softReset()
+    {
+        foreach (Resetable resetableObject in resetableObjects)
+        {
+            resetableObject.reset();
+        }
+        foreach (Resetable startingObject in startingObjects)
+        {
+            startingObject.reset();
+        }
+    }
+
+    public void hardReset()
+    {
+        foreach (Placeable placedObject in placedObjects)
+        {
+            Destroy(placedObject.gameObject);
+        }
+        placedObjects.Clear();
+        resetableObjects.Clear();
+        foreach (Resetable startingObject in startingObjects)
+        {
+            startingObject.reset();
+        }
+    }
+
+    public void goalReached()
+    {
+        softReset();
+        setPaused(true);
+    }
+}
---
+++ Moveable.cs
@@ -0,0 +1,179 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moveable : MonoBehaviour {
+
+    public float xSpeed = 0;
+    public float ySpeed = 0;
+    public float maxWalkSpeed = 0;
+    public float maxRunSpeed = 0;
+    private Rigidbody2D rigidBody;
+    private RaycastHit2D hit;
+    private bool fullWalkSpeed = false;
+    private bool fullRunSpeed = false;
+
+    // Use this for initialization
+    void Start () {
+        rigidBody = gameObject.GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public bool atFullWalkSpeed()
+    {
+        return fullWalkSpeed;
+    }
+
+    public bool atFullRunSpeed()
+    {
+        return fullRunSpeed;
+    }
+
+    public void changeDirection()
+    {
+        xSpeed = -xSpeed;
+    }
+
+    public void moveX()
+    {
+        rigidBody.AddForce(new Vector3(xSpeed, 0), ForceMode2D.Force);
+    }
+
+    public void moveY()
+    {
+        rigidBody.AddForce(new Vector3(0, ySpeed), ForceMode2D.Force);
+    }
+
+    public void moveXY()
+    {
+        rigidBody.AddForce(new Vector3(xSpeed, ySpeed), ForceMode2D.Force);
+    }
+
+    public void walk()
+    {
+        if (xSpeed > 0)
+        {
+            if (rigidBody.velocity.x < maxWalkSpeed)
+            {
+                moveX();
+                if (rigidBody.velocity.x > maxWalkSpeed)
+                {
+                    rigidBody.AddForce(new Vector3(maxWalkSpeed - rigidBody.velocity.x, 0), ForceMode2D.Force);
+                    fullWalkSpeed = true;
+                }
+                else
+                {
+                    fullWalkSpeed = false;
+                }
+            }
+            else
+            {
+                fullWalkSpeed = true;
+            }
+        }
+        else
+        {
+            if (rigidBody.velocity.x > -maxWalkSpeed)
+            {
+                moveX();
+                if (rigidBody.velocity.x < -maxWalkSpeed)
+                {
+                    rigidBody.AddForce(new Vector3(-maxWalkSpeed - rigidBody.velocity.x, 0), ForceMode2D.Force);
+                    fullWalkSpeed = true;
+                }
+                else
+                {
+                    fullWalkSpeed = false;
+                }
+            }
+            else
+            {
+                fullWalkSpeed = true;
+            }
+        }
+    }
+
+    public void run()
+    {
+        if (xSpeed > 0)
+        {
+            if (rigidBody.velocity.x < maxRunSpeed)
+            {
+                moveX();
+                print(rigidBody.velocity.x + " " + maxRunSpeed);
+                if (rigidBody.velocity.x > maxRunSpeed)
+                {
+                    rigidBody.AddForce(new Vector3(maxRunSpeed - rigidBody.velocity.x, 0), ForceMode2D.Force);
+                    fullRunSpeed = true;
+                }
+                else
+                {
+                    fullRunSpeed = false;
+                }
+            }
+            else
+            {
+                fullRunSpeed = true;
+            }
+        }
+        else
+        {
+            if (rigidBody.velocity.x > -maxRunSpeed)
+            {
+                moveX();
+                if (rigidBody.velocity.x < -maxRunSpeed)
+                {
+                    rigidBody.AddForce(new Vector3(-maxRunSpeed - rigidBody.velocity.x, 0), ForceMode2D.Force);
+                    fullRunSpeed = true;
+                }
+                else
+                {
+                    fullRunSpeed = false;
+                }
+            }
+            else
+            {
+                fullRunSpeed = true;
+            }
+        }
+    }
+
+    public void jump()
+    {
+        if (rigidBody.velocity.y < ySpeed)
+        {
+            rigidBody.AddForce(new Vector3(0, ySpeed), ForceMode2D.Impulse);
+            if(rigidBody.velocity.y > ySpeed)
+            {
+                rigidBody.AddForce(new Vector3(0, ySpeed - rigidBody.velocity.y), ForceMode2D.Impulse);
+            }
+            print("jump " + rigidBody.velocity);
+        }
+    }
+
+    public void groundJump()
+    {
+        if(onGround())
+        {
+            jump();
+        }
+    }
+
+    public bool onGround()
+    {
+        hit = Physics2D.Raycast(transform.position, Vector2.down, 0.55f);
+        if(hit.collider != null)
+        {
+            //print(hit.collider.gameObject.name);
+            if (hit.collider.gameObject.tag == "wall")
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
